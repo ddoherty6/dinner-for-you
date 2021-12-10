c7a2f4ea8b7ce0f7886e9f5561c51fd8344f9123
Daniel <ddoherty6@gmail.com>
2021-12-09 21:22:00 -0800

    added to tests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 
 // SET THE environment variables. See http://twil.io/secure
 // require('dotenv').config();
-//console.log("hello");
+// console.log("hello");
 
 const accountString = "AC5e7ccb84fb554959681dfb90d58ed619";
 const tokenString = "f30a3fd6b843a91d091fc39ee32ccb3a";
 
 const accountSid = accountString; // SK038e686467bd4cf0d28cba9c6bea6f83
-const authToken = tokenString; // o557Khe6cmkxt3VXCPA7Frv5B7ScDKDo
-const client = require('twilio')(accountSid, authToken);
+const authToken = tokenString; // some other auth: o557Khe6cmkxt3VXCPA7Frv5B7ScDKDo
+
+require(['twilio'], function(twilio){
+ const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
@@ -17,6 +19,7 @@ client.messages
      to: '+16178238674'
    })
   .then(message => console.log(message.sid))
+});
 
 
   ///research environment vars
