81be77941a3f35059063e31457745f544b057189
Daniel <ddoherty6@gmail.com>
2021-12-09 18:40:03 -0800

    test successful

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 
 // SET THE environment variables. See http://twil.io/secure
 // require('dotenv').config();
+//console.log("hello");
+
 const accountString = "AC5e7ccb84fb554959681dfb90d58ed619";
-const tokenString = "o557Khe6cmkxt3VXCPA7Frv5B7ScDKDo";
+const tokenString = "f30a3fd6b843a91d091fc39ee32ccb3a";
 
 const accountSid = accountString; // SK038e686467bd4cf0d28cba9c6bea6f83
 const authToken = tokenString; // o557Khe6cmkxt3VXCPA7Frv5B7ScDKDo
@@ -14,7 +16,7 @@ client.messages
      from: '+14752557437',
      to: '+16178238674'
    })
-  .then(message => console.log(message.sid));
+  .then(message => console.log(message.sid))
 
 
   ///research environment vars
