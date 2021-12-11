326446b8324e4857db0a93767a8a45eef37735ca
Daniel <ddoherty6@gmail.com>
2021-12-11 13:48:38 -0800

    new API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,29 @@
 
-// SET THE environment variables. See http://twil.io/secure
-// require('dotenv').config();
-// console.log("hello");
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://smtp-e-mail-sender.p.rapidapi.com/v3/smtpjs.aspx",
+	"method": "POST",
+	"headers": {
+		"content-type": "application/json",
+		"x-rapidapi-host": "smtp-e-mail-sender.p.rapidapi.com",
+		"x-rapidapi-key": "61f7e1e89amsh916d43089bd98bep10b853jsn09283b1f8040"
+	},
+	"processData": false,
+	"data": {
+		"Host": "smtp.yourisp.com",
+		"Username": "username",
+		"Password": "password",
+		"To": "them@website.com",
+		"Bcc": "optional@website.com",
+		"From": "you@isp.com",
+		"Subject": "This is the subject",
+		"Body": "And this is the body",
+		"nocache": "441137",
+		"Action": "Send"
+	}
+};
 
-const accountString = "AC5e7ccb84fb554959681dfb90d58ed619";
-const tokenString = "f30a3fd6b843a91d091fc39ee32ccb3a";
-
-const accountSid = accountString; // SK038e686467bd4cf0d28cba9c6bea6f83
-const authToken = tokenString; // some other auth: o557Khe6cmkxt3VXCPA7Frv5B7ScDKDo
-
-require(['twilio'], function(twilio){
- const client = require('twilio')(accountSid, authToken);
-
-client.messages
-  .create({
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+14752557437',
-     to: '+16178238674'
-   })
-  .then(message => console.log(message.sid))
+$.ajax(settings).done(function (response) {
+	console.log(response);
 });
-
-
-  ///research environment vars
