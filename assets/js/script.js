9bdfed06c3f09933f16b8d4d3db6e0560c6bb8e2
Daniel <ddoherty6@gmail.com>
2021-12-13 15:06:20 -0800

    refactored sendMail

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,34 @@
-var    d= {
-  from: {
-      name: "Example",
-      address: "info@example.com"
-  },
-  to: {
-      name: "John Doe",
-      address: "llyybb80@gmail.com"
-  },
-  subject: "This is the mail subject",
-  message: "Hello John, how are you ?"
+var sendMail = function(userName, userEmail, recipeObj) {
+
+  var mailInfo = {
+    from: {
+        name: "Dinner For You",
+        address: "recipe@dinnerforyou.com"
+    },
+    to: {
+        name: userName,
+        address: userEmail,
+    },
+    subject: "This is the mail subject",
+    message: "Hello John, how are you ?"
+  }
+  var form= JSON.stringify(mailInfo);
+
+  const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://easymail.p.rapidapi.com/send",
+    "method": "POST",
+    "headers": {
+        "content-type": "application/json",
+        "x-rapidapi-host": "easymail.p.rapidapi.com",
+        "x-rapidapi-key": "79bc5e5863msh3ef58438b7034ddp1e35ddjsn2401862fc36a"
+    },
+    "processData": false,
+    "data":form
+  };
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
 }
-var form= JSON.stringify(d)
-const settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://easymail.p.rapidapi.com/send",
-  "method": "POST",
-  "headers": {
-      "content-type": "application/json",
-      "x-rapidapi-host": "easymail.p.rapidapi.com",
-      "x-rapidapi-key": "79bc5e5863msh3ef58438b7034ddp1e35ddjsn2401862fc36a"
-  },
-  "processData": false,
-  "data":form
-};
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
