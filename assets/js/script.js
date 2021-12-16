ae04d121b6ceb20054dada890fa9a681b96b2ef2
Daniel <ddoherty6@gmail.com>
2021-12-16 14:29:02 -0800

    finished modal, implemented function to demo email API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
-var sendMail = function(userName, userEmail) {
+var sendMail = function(userName, userEmail) { // using EasyMail API
 
+  // email is sent successfully
+
+  setInterval(function() {
+    $('#message').text("Email sent successfully!");
+    $('#message').addClass("email-sent");
+  }, 2500)
+
+  debugger;
+
+  console.log(writeMail());
+ 
+
+
+
+
+  /*
   meal = $("#meal").text();  
   bodyText = writeMail();
 
@@ -18,7 +34,7 @@ var sendMail = function(userName, userEmail) {
 
   var form= JSON.stringify(mailInfo);
 
-  const settings = {
+  const settings = { // API information object
     "async": true,
     "crossDomain": true,
     "url": "https://easymail.p.rapidapi.com/send",
@@ -29,12 +45,28 @@ var sendMail = function(userName, userEmail) {
         "x-rapidapi-key": "79bc5e5863msh3ef58438b7034ddp1e35ddjsn2401862fc36a"
     },
     "processData": false,
-    "data":form
+    "data": form
   };
 
   $.ajax(settings).done(function (response) {
     console.log(response);
-  });
+    /*if (response.ok) {
+
+      // email is sent successfully
+      $('#message').text("Email sent successfully!");
+      $('#message').addClass("email-sent");
+
+      $('<span><a id="close" class="modal-close btn">Close</a></span>')
+			.appendTo(".modal-content");
+    } else {
+      // email send fails
+      $('#message').text("Email was unsuccessful :(");
+      $('#message').addClass("email-fail");
+
+      $('<span><a id="close" class="modal-close btn">Close</a></span>')
+			.appendTo(".modal-content");
+    }
+  }); */
 }
 
 var writeMail = function() { // creates text content of HTML email
@@ -53,47 +85,61 @@ var writeMail = function() { // creates text content of HTML email
   for (var i = 0; i < $("#in").children().length || i < $("#in").children().length; i++) {
 
     tableEl = $("#in").children()[i];
-    
-
     measureEl = $("#me").children()[i];
-
-
     tableRows[i] = "<tr><td>" + tableEl.innerText + "</td><td>" + measureEl.innerText + "</td></tr>";
 
   }
 
-  var headers = "<h1>"+meal+"</h1><h2>"+region+"</h2><h3>"+category+"</h3><h4>Ingredients</h4>";
+  var headers = "<h1>"+meal+"</h1><h2>"+region+"</h2><h3>"+category+"</h3><h4>Ingredients</h4>"; // construct email as html elements
+  var ingredientTable = "<table>"+tableRows.join("")+"</table>";                                 //
+  var instructions = "<h4>Instructions</h4><p>" + $("#mea p").text() + "</p>";                   //
+  var footer = "<p>&copy; Dinner For You LLC Infinity</p>"                                       //
 
-  var ingredientTable = "<table>"+tableRows.join("")+"</table>";
-  console.log(ingredientTable);
-
-  var instructions = "<h4>Instructions</h4><p>" + $("#mea p").text() + "</p>";
-
-  var footer = "<p>&copy; Dinner For You LLC Infinity</p>"
-
-  var bodyText = headers + ingredientTable + instructions + footer;
+  var bodyText = headers + ingredientTable + instructions + footer; // assemble parts of email into final body
 
   return bodyText;
 }
 
-/*
-$('#email-me').on("click", function(event) {
-  event.preventDefault();
-  $("#modal1").attr("class", "btn");
-});
+// modal UI triggers
 
-// modal was triggered
-$(".modal").on("show.bs.modal", function() {
-  // clear values
-  $(".modal-close .waves-effect .waves-green .btn-flat").val("");
-});
-
-// modal is fully visible
-$(".modal").on("shown.bs.modal", function() {
-  // highlight textarea
-  $(".modal-close .waves-effect .waves-green .btn-flat").trigger("focus");
-}); */
-
-$(document).ready(function(){
+$(document).ready(function() {
   $('.modal').modal();
 });
+
+$("#send").on("click", function() { // pulls name and email from modal, sends the email
+  var name = $("#name").val();
+  console.log(name);
+  var email = $("#email").val();
+  console.log(email);
+
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) { // validating email address
+    debugger;
+    // email address is validated:
+    console.log("Valid");
+
+    $('#message').text("Email validated, sending...");
+    $('#message').attr("class", "helper-text"); // alert the user we are sending the email
+
+    $("#close").on("click", function() {
+      $('#message').text(""); // reset message text
+      $('#message').attr("class", "helper-text");
+    });
+
+    sendMail(name, email); //send the emaail
+    debugger;
+
+  } else {
+    // email address fails validation:
+    console.log("Validation failed");
+    $('#message').text("Email is invald. Please enter a valid Email address.");
+    $('#message').addClass("email-fail");
+  }
+});
+
+$("#email-me").on("click", function() {
+  debugger;
+  $("#name").text("");
+  $('#message').text("");
+  $("#email").text("");
+})
+
